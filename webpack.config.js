b26dc48cb3e27f1094aecf0b9e46e3380a4d7fd1
Félicien Brochu <felicien.brochu@gmail.com>
2019-04-19 16:04:11 +0200

    Clean webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,31 @@
-const {
-  VueLoaderPlugin
-} = require('vue-loader');
 var webpack = require('webpack')
-const nodeSassMagicImporter = require('node-sass-magic-importer');
-const path = require('path');
+const path = require('path')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const VueLoader = require('vue-loader')
 
 const env = process.env.NODE_ENV;
-const minify = env === 'production';
-const sourceMap = env === 'development';
 
 const config = {
-  entry: [
-    path.join(__dirname, 'src', 'main.js')
-  ],
+  entry: {
+    app: [path.resolve(__dirname, 'src/main.js')]
+  },
   mode: env,
   output: {
     publicPath: '/',
   },
   optimization: {
-    // splitChunks: {
-    //   // Must be specified
-    //   // for HtmlWebpackPlugin to work correctly.
-    //   // See: https: //github.com/jantimon/html-webpack-plugin/issues/882
-    //   chunks: 'all'
-    // },
+    splitChunks: {
+      chunks: 'all'
+    },
   },
-  devtool: sourceMap ? 'cheap-module-eval-source-map' : undefined,
+  devtool: env === 'development' ? 'inline-source-map' : undefined,
+  devServer: {
+    contentBase: './dist'
+  },
   module: {
     rules: [{
         test: /\.vue$/,
@@ -51,49 +44,24 @@ const config = {
         test: /\.scss$/,
         use: [
           'vue-style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap,
-            },
-          },
+          'css-loader',
+          'sass-loader'
         ],
       },
     ],
   },
   plugins: [
-    new VueLoaderPlugin(),
+    new VueLoader.VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: path.join(__dirname, 'dist', 'index.html'),
       template: path.join(__dirname, 'static', 'index.html'),
-      inject: true,
-      minify: minify ? {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true,
-        // More options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      } : false,
+      inject: true
     }),
     new CopyWebpackPlugin([{
       from: 'static/assets',
       to: 'assets'
-    }]),
-    new BrowserSyncPlugin({
-      // browse to http://localhost:3000/ during development,
-      // ./public directory is being served
-      host: 'localhost',
-      port: 3000,
-      server: {
-        baseDir: ['dist']
-      }
-    })
+    }])
   ],
   resolve: {
     alias: {
@@ -101,13 +69,10 @@ const config = {
     },
     extensions: ['*', '.js', '.vue', '.json']
   },
-};
+}
 
-if (minify) {
+if (env === 'production') {
   config.optimization.minimizer = [
-    new OptimizeCSSAssetsPlugin(),
-    // Enabled by default in production mode if
-    // the 'minimizer' option isn't overridden.
     new UglifyJsPlugin({
       cache: true,
       parallel: true,
@@ -117,18 +82,7 @@ if (minify) {
         },
       },
     }),
-  ];
-}
-
-if (env !== 'development') {
-  config.plugins.push(new MiniCssExtractPlugin());
-
-  const sassLoader = config.module.rules.find(({
-    test
-  }) => test.test('.scss'));
-  // Replace the 'vue-style-loader' with
-  // the MiniCssExtractPlugin loader.
-  sassLoader.use[0] = MiniCssExtractPlugin.loader;
+  ]
 }
 
 module.exports = config;
