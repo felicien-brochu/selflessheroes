22fc1e592c36150b2a557e147a560f1bd6ed1907
Félicien Brochu <felicien.brochu@gmail.com>
2019-08-29 10:03:49 +0200

    Arrows and hero icon on DirectionPopup.

--- src/components/level/grapheditor/nodes/DirectionPopup.vue
+++ src/components/level/grapheditor/nodes/DirectionPopup.vue
@@ -1,13 +1,27 @@
 <template>
 <div :class="['popup', 'direction-popup', colorClass]">
-  <ul class="direction-values">
+  <ul class="direction-tiles">
     <li v-for="dir in directionNames"
-      :class="{'selected': selectedDirectionNames.includes(dir), 'no-value': notHere && dir === 'here'}"
+      :class="{
+				'selected': selectedDirectionNames.includes(dir),
+				'no-value': notHere && dir === 'here'
+			}"
       @click="handleDirectionClick(dir)">
       <button class="tile" />
+      <div :class="{
+				'tile-icon': true,
+				'tile-nw': dir === 'nw',
+				'tile-n': dir === 'n',
+				'tile-ne': dir === 'ne',
+				'tile-w': dir === 'w',
+				'tile-here': dir === 'here',
+				'tile-e': dir === 'e',
+				'tile-sw': dir === 'sw',
+				'tile-s': dir === 's',
+				'tile-se': dir === 'se'
+			}" />
     </li>
   </ul>
-  </table>
 </div>
 </template>
 
@@ -98,20 +112,25 @@ export default {
 .direction-popup {
     border-radius: 5px;
 
-    .direction-values {
+    .direction-tiles {
         width: 92px;
         height: 92px;
-        li {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-content: space-between;
 
-            background: none;
+        li {
             display: flex;
+            position: relative;
             justify-content: center;
             align-items: center;
             height: 30px;
             width: 30px;
 
             &.no-value .tile {
-                background: none !important;
+                display: none;
             }
 
             &:nth-child(3n) {
@@ -131,6 +150,52 @@ export default {
                 align-items: flex-start;
             }
 
+            .tile-icon {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: 57%;
+            }
+
+            .tile-here {
+                background-image: url("../../../images/hero-icon.png");
+                background-size: 68%;
+            }
+            .tile-nw {
+                background-image: url("../../../images/direction-nw-icon.png");
+                background-position: left 73% top 73%;
+            }
+            .tile-n {
+                background-image: url("../../../images/direction-n-icon.png");
+                background-position: left 50% top 70%;
+            }
+            .tile-ne {
+                background-image: url("../../../images/direction-ne-icon.png");
+                background-position: right 73% top 73%;
+            }
+            .tile-w {
+                background-image: url("../../../images/direction-w-icon.png");
+                background-position: top 50% left 70%;
+            }
+            .tile-e {
+                background-image: url("../../../images/direction-e-icon.png");
+                background-position: top 50% right 70%;
+            }
+            .tile-sw {
+                background-image: url("../../../images/direction-sw-icon.png");
+                background-position: left 73% bottom 73%;
+            }
+            .tile-s {
+                background-image: url("../../../images/direction-s-icon.png");
+                background-position: left 50% bottom 70%;
+            }
+            .tile-se {
+                background-image: url("../../../images/direction-se-icon.png");
+                background-position: right 73% bottom 73%;
+            }
+
             .tile {
                 background-color: transparentize(white, 0.75);
                 width: 24px;
@@ -147,7 +212,6 @@ export default {
 
             &.selected,
             &:active {
-                background: none;
                 .tile {
                     background-color: transparentize(white, 0.2);
                 }
