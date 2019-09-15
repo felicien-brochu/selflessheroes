28c0dbf3ad7ba6d7bb558ab2eb880899fe3e56c0
Félicien Brochu <felicien.brochu@gmail.com>
2019-09-15 16:20:08 +0200

    Change Level11 objective message.

--- src/levels/level11/Level11.js
+++ src/levels/level11/Level11.js
@@ -39,8 +39,8 @@ const level = {
     fr: "Courage, fuyons\u00A0!",
   },
   objective: {
-    en: "Don't die!\n\nThese two ogres are after you, get rid of them before they catch up with you.\n\n%%icon mdi mdi-information-outline$%% Use the %%icon icon-switch$%% switches to disable/enable the %%icon icon-spikes$%% spikes.",
-    fr: "Ne meurs pas!\n\nCes deux ogres en ont après toi, débarrasse t'en avant qu'ils ne te rattrapent.\n\n%%icon mdi mdi-information-outline$%% Utilise les %%icon icon-switch$%% boutons pour activer/désactiver les %%icon icon-spikes$%% piques.",
+    en: "Don't die!\n\nThese two ogres are after you. Get rid of them before they catch up with you.\n\n%%icon mdi mdi-information-outline$%% Use the %%icon icon-switch$%% switches to disable/enable the %%icon icon-spikes$%% spikes.",
+    fr: "Ne meurs pas!\n\nCes deux ogres en ont après toi. Débarrasses-en-toi avant qu'ils ne te rattrapent.\n\n%%icon mdi mdi-information-outline$%% Utilise les %%icon icon-switch$%% boutons pour activer/désactiver les %%icon icon-spikes$%% piques.",
   },
   startingCode: "",
   startingEditorType: "graph",
