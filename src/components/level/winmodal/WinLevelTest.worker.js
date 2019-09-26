b4f535e3f5c5648c880a68f27c450f595b508618
Félicien Brochu <felicien.brochu@gmail.com>
2019-09-26 17:03:36 +0200

    Get test code in a dedicated class.

--- src/components/level/winmodal/WinLevelTest.worker.js
+++ src/components/level/winmodal/WinLevelTest.worker.js
@@ -1,112 +1,18 @@
-import seedrandom from 'seedrandom'
-
 import levelManager from '../../../levels/levelManager'
-import World from '../../../world/World'
-import Compiler from '../../../world/ai/compile/Compiler'
+import Tester from '../../../world/Tester'
 
 const sampleSize = 20
 
-self.addEventListener('message', function(e) {
+self.addEventListener('message', e => {
   const level = levelManager.getLevelByID(e.data.levelID)
   const code = e.data.code
-  testCode(level, code)
-})
 
-function testCode(level, code) {
-  const compiler = new Compiler(code, level.buildCompilerConfig())
-  const aiFactory = compiler.compile()
-
-  let tests = []
-  for (let i = 0; i < sampleSize; i++) {
-    let {
-      rng,
-      seed
-    } = buildRNG()
-    let world = new World(level, aiFactory, rng)
-    while (!world.gameOver) {
-      world.step()
-    }
-
-    let lossReason = null
-    if (world.hasLost) {
-      lossReason = world.ruleset.getLossReason()
-    }
-    tests.push({
-      steps: world.steps,
-      rngSeed: seed,
-      hasLost: world.hasLost,
-      lossReason: lossReason,
-    })
-  }
+  let tester = new Tester(level, code, sampleSize)
+  let tests = tester.test()
 
   if (SHOW_STATS) {
-    printStats(tests)
+    tester.printStats()
   }
 
   postMessage(tests)
-}
-
-function buildRNG() {
-  return seedrandom(null, {
-    pass: (rng, seed) => {
-      return {
-        rng: rng,
-        seed: seed
-      }
-    }
-  })
-}
-
-function printStats(tests) {
-  let avg = tests.reduce((acc, test) => acc + test.steps, 0) / tests.length
-  let max = tests.reduce((acc, test) => Math.max(acc, test.steps), 0)
-  let min = tests.reduce((acc, test) => Math.min(acc, test.steps), Infinity)
-  let lost = tests.reduce((acc, test) => test.hasLost ? acc + 1 : acc, 0)
-  let variance = tests.reduce((acc, test) => acc + (test.steps - avg) ** 2, 0) / tests.length
-  let maxAvg = 0
-  let minAvg = Infinity
-  let seriesAverage = []
-  let lostSeries = 0
-
-  for (let i = 0; i < sampleSize - 20 + 1; i++) {
-    let seriesAvg = 0
-    let lost = false
-    for (let j = i; j < i + 20; j++) {
-      seriesAvg += tests[j].steps
-
-      if (!lost && tests[j].hasLost) {
-        lostSeries++
-        lost = true
-      }
-    }
-    seriesAvg /= 20
-    seriesAverage.push(seriesAvg)
-    maxAvg = Math.max(maxAvg, seriesAvg)
-    minAvg = Math.min(minAvg, seriesAvg)
-  }
-  seriesAverage.sort((a, b) => a - b)
-
-  console.debug("//// tests stats ////")
-  console.debug("sample:", sampleSize)
-  console.debug("avg:   ", avg)
-  console.debug("max:   ", max, ` (${(max - avg) / Math.sqrt(variance)} σ)`)
-  console.debug("min:   ", min, ` (${(avg - min) / Math.sqrt(variance)} σ)`)
-  console.debug("lost:  ", `${(lost / sampleSize) * 100}%`)
-  console.debug("lostSr:", `${(lostSeries / (sampleSize - 20 + 1)) * 100}%`)
-  console.debug("σ:     ", Math.sqrt(variance))
-  console.debug("σ²:    ", variance)
-  console.debug("max 20:", maxAvg, `(over ${sampleSize - 20 + 1} samples)`)
-  console.debug("10%   :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.9)])
-  console.debug("5%    :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.95)])
-  console.debug("1%    :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.99)])
-  console.debug(".5%   :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.995)])
-  console.debug(".1%   :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.999)])
-  // console.debug("series:", seriesAverage)
-  // for (let point of seriesAverage) {
-  //   let graph = ''
-  //   for (let i = 0; i < point - minAvg; i++) {
-  //     graph += '|'
-  //   }
-  //   console.log(graph + '  ' + point)
-  // }
-}
+})
---
+++ src/world/Tester.js
@@ -0,0 +1,133 @@
+import seedrandom from 'seedrandom'
+
+import World from './World'
+import Compiler from './ai/compile/Compiler'
+
+export default class Tester {
+
+  constructor(level, code, sampleSize, targetSampleSize = 20) {
+    this.level = level
+    this.code = code
+    this.sampleSize = sampleSize
+    this.targetSampleSize = targetSampleSize
+
+    this.compiler = new Compiler(code, level.buildCompilerConfig())
+    this.aiFactory = this.compiler.compile()
+    this.tests = []
+  }
+
+  test() {
+    this.tests = []
+
+    for (let i = 0; i < this.sampleSize; i++) {
+      let {
+        rng,
+        seed
+      } = Tester.buildRNG()
+      let world = new World(this.level, this.aiFactory, rng)
+      while (!world.gameOver) {
+        world.step()
+      }
+
+      this.tests.push({
+        steps: world.steps,
+        rngSeed: seed,
+        hasLost: world.hasLost,
+        lossReason: world.lossReason,
+      })
+    }
+
+    return this.tests
+  }
+
+  static buildRNG() {
+    return seedrandom(null, {
+      pass: (rng, seed) => {
+        return {
+          rng: rng,
+          seed: seed
+        }
+      }
+    })
+  }
+
+  getStats() {
+    let avg = this.tests.reduce((acc, test) => acc + test.steps, 0) / this.tests.length
+    let max = this.tests.reduce((acc, test) => Math.max(acc, test.steps), 0)
+    let min = this.tests.reduce((acc, test) => Math.min(acc, test.steps), Infinity)
+    let lost = this.tests.reduce((acc, test) => test.hasLost ? acc + 1 : acc, 0)
+    let variance = this.tests.reduce((acc, test) => acc + (test.steps - avg) ** 2, 0) / this.tests.length
+    let maxAvg = 0
+    let minAvg = Infinity
+    let seriesAverage = []
+    let lostSeries = 0
+
+    for (let i = 0; i < this.sampleSize - this.targetSampleSize + 1; i++) {
+      let seriesAvg = 0
+      let lost = false
+      for (let j = i; j < i + this.targetSampleSize; j++) {
+        seriesAvg += this.tests[j].steps
+
+        if (!lost && this.tests[j].hasLost) {
+          lostSeries++
+          lost = true
+        }
+      }
+      seriesAvg /= this.targetSampleSize
+      seriesAverage.push(seriesAvg)
+      maxAvg = Math.max(maxAvg, seriesAvg)
+      minAvg = Math.min(minAvg, seriesAvg)
+    }
+    seriesAverage.sort((a, b) => a - b)
+
+    return {
+      avg,
+      max,
+      min,
+      lost,
+      variance,
+      maxAvg,
+      minAvg,
+      seriesAverage,
+      lostSeries,
+    }
+  }
+
+  printStats() {
+    let {
+      avg,
+      max,
+      min,
+      lost,
+      variance,
+      maxAvg,
+      minAvg,
+      seriesAverage,
+      lostSeries,
+    } = this.getStats()
+
+    console.debug("//// tests stats ////")
+    console.debug("sample:", this.sampleSize)
+    console.debug("avg:   ", avg)
+    console.debug("max:   ", max, ` (${(max - avg) / Math.sqrt(variance)} σ)`)
+    console.debug("min:   ", min, ` (${(avg - min) / Math.sqrt(variance)} σ)`)
+    console.debug("lost:  ", `${(lost / this.sampleSize) * 100}%`)
+    console.debug("lostSr:", `${(lostSeries / (this.sampleSize - this.targetSampleSize + 1)) * 100}%`)
+    console.debug("σ:     ", Math.sqrt(variance))
+    console.debug("σ²:    ", variance)
+    console.debug("max 20:", maxAvg, `(over ${this.sampleSize - this.targetSampleSize + 1} samples)`)
+    console.debug("10%   :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.9)])
+    console.debug("5%    :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.95)])
+    console.debug("1%    :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.99)])
+    console.debug(".5%   :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.995)])
+    console.debug(".1%   :", seriesAverage[Math.floor((seriesAverage.length - 1) * 0.999)])
+    // console.debug("series:", seriesAverage)
+    // for (let point of seriesAverage) {
+    //   let graph = ''
+    //   for (let i = 0; i < point - minAvg; i++) {
+    //     graph += '|'
+    //   }
+    //   console.log(graph + '  ' + point)
+    // }
+  }
+}
