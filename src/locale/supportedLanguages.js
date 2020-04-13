71bc8e44e070984c1fe6be0c13a269e95b364998
Félicien Brochu <felicien.brochu@gmail.com>
2020-04-13 10:58:43 +0200

    Enable portuguese.

--- src/locale/supportedLanguages.js
+++ src/locale/supportedLanguages.js
@@ -1,11 +1,11 @@
 export default [
   'en',
   'fr',
+  'pt',
   // Uncomment one of the following lines to enable the corresponding language
   // 'ar',
   // 'de',
   // 'es',
   // 'it',
   // 'pl',
-  // 'pt',
 ]
