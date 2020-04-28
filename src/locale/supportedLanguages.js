6d1653356121ea051635e7d17bc68cab96421976
Félicien Brochu <felicien.brochu@gmail.com>
2020-04-28 13:15:09 +0200

    Enable german language.

--- src/locale/supportedLanguages.js
+++ src/locale/supportedLanguages.js
@@ -1,11 +1,11 @@
 export default [
+  'de',
   'en',
   'fr',
   'pl',
   'pt',
   // Uncomment one of the following lines to enable the corresponding language
   // 'ar',
-  // 'de',
   // 'es',
   // 'it',
 ]
