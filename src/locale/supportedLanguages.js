82573c6f5a284fa7bbcd40c9394f9246ccb38a5f
Félicien Brochu <felicien.brochu@gmail.com>
2020-06-09 10:24:29 +0200

    Enable spanish language.

--- src/locale/supportedLanguages.js
+++ src/locale/supportedLanguages.js
@@ -1,13 +1,13 @@
 export default [
   'de',
   'en',
+  'es',
   'fr',
   'pl',
   'pt',
   // Uncomment one of the following lines to enable the corresponding language
   // 'ar',
   // 'eo',
-  // 'es',
   // 'it',
   // 'ru',
 ]
