2c9ebff38cc09f3e2bad91e1701c6c0e94822b06
Félicien Brochu <felicien.brochu@gmail.com>
2021-05-07 13:26:14 +0200

    Enable Russian language.

--- src/locale/supportedLanguages.js
+++ src/locale/supportedLanguages.js
@@ -5,10 +5,10 @@ export default [
   'fr',
   'pl',
   'pt',
+  'ru',
   'sv',
   // Uncomment one of the following lines to enable the corresponding language
   // 'ar',
   // 'eo',
   // 'it',
-  // 'ru',
 ]
